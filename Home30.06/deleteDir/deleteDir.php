bff771b3f614211617013e8a9034e6645d66fec4
Philip <heroesfill@gmail.com>
2020-06-30 12:26:39 +0300

    Попытка №2 удалить директорию

--- Home30.06/deleteDir/deleteDir.php
+++ Home30.06/deleteDir/deleteDir.php
@@ -1,19 +1,31 @@
 <?php
 
 function deleteDir($name_dir){
-    if ($objs = glob($name_dir."/*")) {
-        foreach($objs as $obj) {
-          is_dir($obj) ? deleteDir($obj) : unlink($obj);
-        }
-     }
-     rmdir($name_dir);}
+  if ($objs = glob($name_dir."/*")) {
+     foreach($objs as $obj) {
+      is_dir($obj) ? deleteDir($obj) : unlink($obj);
+    }
+  }
+rmdir($name_dir);}
+
 //  работает, но состаавил не сам нашёл на формуме...
-
-
 // deleteDir('dir1');
 
-function delDir($name_dir){
-    if(is_dir($name_dir)){
-        // сам пока не смог составить функцию 
-    }
-}
+
+// Эта пока не работает, но я вернусь) 
+// function delDir($dir){
+//   if(is_dir($dir)){
+//     if(($dh=opendir($dir))!==false){
+//       while(($file=readdir($dh))!==false){
+//         if(is_file($file)){ 
+//           unlink($file);
+//         }else {
+//           delDir($file);
+//         }  
+//       }
+//     }
+//     closedir($dh);
+//     rmdir($dir);
+//   }
+// }
+// delDir('deldir1');
